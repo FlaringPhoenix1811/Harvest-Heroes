da823e3b026e4c16fc0bd887e75388fe160fd66e
Meet Jain <115941907+FlaringPhoenix1811@users.noreply.github.com>
2023-09-23 14:47:06 +0530

    Add files via upload

---
+++ HarvestHeroes/GameAssets/Scripts/UI/Inventory_UI.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory_UI : MonoBehaviour
+{
+    public GameObject inventoryPanel;
+    public Player player;
+    public List<Slot_UI> slots = new List<Slot_UI>();
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleInventory();
+        }
+    }
+
+    public void ToggleInventory()
+    {
+        if(!inventoryPanel.activeSelf)
+        {
+            inventoryPanel.SetActive(true);
+            Refresh();
+        }
+        else
+        {
+            inventoryPanel.SetActive(false);
+        }
+    }
+
+    void Refresh()
+    {
+        if(slots.Count == player.inventory.slots.Count)
+        {
+            for(int i = 0; i < slots.Count; i++)
+            {
+                if(player.inventory.slots[i].type != CollectableType.NONE)
+                {
+                    slots[i].SetItem(player.inventory.slots[i]);
+                }
+                else
+                {
+                    slots[i].SetEmpty();
+                }
+            }
+        }
+    }
+
+    public void Remove(int slotID)
+    {
+        Collectable itemToDrop = GameManager.instance.itemManager.GetItemByType(player.inventory.slots[slotID].type);
+        if(itemToDrop != null)
+        {
+            player.DropItem(itemToDrop);
+            player.inventory.Remove(slotID);
+            Refresh();
+        }
+
+    }
+}
